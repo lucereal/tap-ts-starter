3a0def4271d98d77a4cfbc2bfdaeff1f0d66cb72
David Lucero <david.lucero@ttu.edu>
2018-07-10 14:49:31 -0500

    changed test/parse-testdata.test.ts

--- test/parse-testdata.test.ts
+++ test/parse-testdata.test.ts
@@ -1,21 +1,19 @@
 import { parseItem } from '../src/tap-main'
 import * as fse from 'fs-extra'
-import each from 'jest-each'
-import { parse } from 'path'
 
 const inputDir = 'testdata/tests'
 const resultDir = 'testdata/expectedResults'
 const config = 'test/testdata-config.json'
 
-async function readConfigs(configuration) {
-  try {
-    const packageObj = await fse.readJson(configuration)
-    return packageObj
-  } catch (err) {
-    console.error(err)
-    return {}
-  }
-}
+// async function readConfigs(configuration) {
+//   try {
+//     const packageObj = await fse.readJson(configuration)
+//     return packageObj
+//   } catch (err) {
+//     console.error(err)
+//     return {}
+//   }
+// }
 
 /*
 test('checking parseItem.', async () => {
@@ -46,63 +44,141 @@ test('checking parseItem.', async () => {
 })
 */
 
-let configObj
-let parsedemailArray = []
-let expectedArray = []
-let expected
-let parsedemail
-let index
+// let configObj
+// let parsedemailArray = []
+// let expectedArray = []
+// let expected
+// let parsedemail
+// let index
 
-async function fish() {
-  console.log('here')
-  configObj = await readConfigs(config)
 
-  let email = await fse.readFile(inputDir + '/' + configObj[0].testdata) //contains email in form of buffer
-  parsedemail = await parseItem(email) // contains parsed email in form of RECORD json
-  expected = await fse.readJson(resultDir + '/' + configObj[0].expectedresult) //contains expected json
+// let inoutArr = [["dog","dogs"],["a","b"]];
 
-  parsedemail = JSON.stringify(parsedemail)
-  expected = JSON.stringify(expected)
-  console.log('***************')
-  //return [parsedemail,expected];
-  return ['A', 'B']
-}
-//fish();
+// describe('test', async () => {
+//   let inp = 'ishhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'
+//   let out = 'fishhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'
+//   let inputArr = []
+//   let a;
+//   let b;
+//   let outArr = []
+//   outArr.push(out)
+//   inputArr.push('dog')
+//   outArr.push('dogg')
 
-describe('test', async () => {
-  let inp = 'ishhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'
-  let out = 'fishhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh'
-  let inputArr = []
+  
+//   //inoutArr.push([inp,out]);
 
-  let outArr = []
-  outArr.push(out)
-  inputArr.push('dog')
-  outArr.push('dogg')
+//   //console.log(inoutArr[0] + '*')
+//   //let index = 0;
+//   //inoutArr.push(await fish());
+//   //configObj = await readConfigs(config);
 
-  let inoutArr = []
-  //inoutArr.push([inp,out]);
+//   beforeAll(async () => {
+//     // inoutArr.push(['dog', 'dogg'])
+//     // let a = await fish()
+//     // console.log(a)
 
-  console.log(inoutArr[0] + '*')
+//     // inoutArr.push(a)
 
-  //inoutArr.push(await fish());
-  //configObj = await readConfigs(config);
+//     // console.log(inoutArr.length)
 
-  beforeAll(async () => {
-    inoutArr.push(['dog', 'dogg'])
-    let a = await fish()
-    console.log(a)
+//     // inputArr.push(inp)
+//   })
+//   beforeEach(() =>{
+//     inputArr.push(fish());
+//     console.log("beforeEach");
 
-    inoutArr.push(a)
+//   })
 
-    console.log(inoutArr.length)
+//   test.each(inoutArr)('testing with ', (input, expected) => {
+//     console.log('\nLENGTH = ' + inoutArr.length + '\n')
+    
 
-    inputArr.push(inp)
-  })
+//     expect(input).toBe(expected)
+//   })
+// })
+// let parsedemail;
+// let expected;
 
-  test.each(inoutArr)('testing with ', (input, expected) => {
-    console.log(inoutArr.length)
-    console.log(inoutArr[2])
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//test.eml////////////with/////////////test.json//////////////////////////////////////////////////////
+test('testing parse mime', async () => {
 
-    expect(input).toBe(expected)
-  })
-})
+  let testdata = 'test.eml';
+  let expectedresult = 'test.json'
+
+  
+  console.log('testing ' +testdata+ ' with ' +expectedresult);
+  let email = await fse.readFile(inputDir + '/' + testdata) //contains email in form of buffer
+  let parsedemail = await parseItem(email) // contains parsed email in form of RECORD json
+  let expected = await fse.readJson(resultDir + '/' + expectedresult) //contains expected json
+  expected.time_extracted = parsedemail.time_extracted.toISOString()
+
+  parsedemail = JSON.parse(JSON.stringify(parsedemail));
+  expected = JSON.parse(JSON.stringify(expected));
+
+  expect(parsedemail).toMatchObject(expected);
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//testA.eml////////////with/////////////testA.json//////////////////////////////////////////////////////
+test('testing parse mime', async () => {
+
+  let testdata = 'testA.eml';
+  let expectedresult = 'testA.json'
+
+  
+  console.log('testing ' +testdata+ ' with ' +expectedresult);
+  let email = await fse.readFile(inputDir + '/' + testdata) //contains email in form of buffer
+  let parsedemail = await parseItem(email) // contains parsed email in form of RECORD json
+  let expected = await fse.readJson(resultDir + '/' + expectedresult) //contains expected json
+  expected.time_extracted = parsedemail.time_extracted.toISOString()
+
+  parsedemail = JSON.parse(JSON.stringify(parsedemail));
+  expected = JSON.parse(JSON.stringify(expected));
+
+  expect(parsedemail).toMatchObject(expected);
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//testB.eml////////////with/////////////testB.json//////////////////////////////////////////////////////
+test('testing parse mime', async () => {
+
+  let testdata = 'testB.eml';
+  let expectedresult = 'testB.json'
+
+  
+  console.log('testing ' +testdata+ ' with ' +expectedresult);
+  let email = await fse.readFile(inputDir + '/' + testdata) //contains email in form of buffer
+  let parsedemail = await parseItem(email) // contains parsed email in form of RECORD json
+  let expected = await fse.readJson(resultDir + '/' + expectedresult) //contains expected json
+  expected.time_extracted = parsedemail.time_extracted.toISOString()
+
+  parsedemail = JSON.parse(JSON.stringify(parsedemail));
+  expected = JSON.parse(JSON.stringify(expected));
+
+  expect(parsedemail).toMatchObject(expected);
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//testC.eml////////////with/////////////testC.json//////////////////////////////////////////////////////
+test('testing parse mime', async () => {
+
+  let testdata = 'testA.eml';
+  let expectedresult = 'testA.json'
+
+  
+  console.log('testing ' +testdata+ ' with ' +expectedresult);
+  let email = await fse.readFile(inputDir + '/' + testdata) //contains email in form of buffer
+  let parsedemail = await parseItem(email) // contains parsed email in form of RECORD json
+  let expected = await fse.readJson(resultDir + '/' + expectedresult) //contains expected json
+  expected.time_extracted = parsedemail.time_extracted.toISOString()
+
+  parsedemail = JSON.parse(JSON.stringify(parsedemail));
+  expected = JSON.parse(JSON.stringify(expected));
+
+  expect(parsedemail).toMatchObject(expected);
+});
